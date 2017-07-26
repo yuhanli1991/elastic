db29c3451f4ae4fc7eca9ccea1beda44b2fceea4
yuhan.l.li <yuhan.l.li@Oracle.com>
2017-07-26 18:12:17 +0800

    correct containsSpace

--- src/main/java/elk/elastic/App.java
+++ src/main/java/elk/elastic/App.java
@@ -30,56 +30,56 @@ public class App
 //    	System.out.println(Snippet.getCompPrefix("clssgmCopyoutMemberInfo(): packed memberNo(0) grock(#CLSS_CONFIG_LOCK) nodeNum(1) privateDataSize(0) publicDataSize(0) 0x0, dead 0, dereg 0, orphan 0"));
 //    	System.out.println(Snippet.getCompPrefixForMap("clssgmCopyoutMemberInfo\\(.*\\): packed memberNo\\(.*\\) grock\\(.*\\) nodeNum\\(.*\\) privateDataSize\\(.*\\) publicDataSize\\(.*\\) \\S+ \\S+ \\S+ dereg \\S+ orphan \\S+"));
     	
-    	extract e = new extract();
-    	System.out.println(e.cutLine("Start Date               ")[2]);
+//    	extract e = new extract();
+//    	System.out.println(e.cutLine("Start Date               ")[2]);
     	
     	
     	
-//    	String[] nodes = args[8].split(",");
-//    	extract e = new extract(args[0], args[1], args[2]);
-//		if (args[3].equals("addtmp")) {
-//			EsClient ec = new EsClient();
-//			List<List<String>> snippet;
-//			
-//			try {
-//				snippet = ec.getSnippet(nodes, args[2], args[5], args[6], "rws00fxw-cluster", "rws00fxw.us.oracle.com", 9300, args[9]);
-//				List<String> content = snippet.get(0);
-//				content = e.removeDupList(content);
-//				Collections.sort(content);
-//				System.out.println("Using " + content.size() + " lines of logs");
-//				e.addTmp(content);
-//				//return Snippet.scoreLog(judge.getScore(), snippet.get(1), logType, templatesFile, scoreSet);
-//			} catch (UnknownHostException e1) {
-//				// TODO Auto-generated catch block
-//				e1.printStackTrace();
-//			}
-//			
-//			System.out.println("Adding templates completed, please check " + args[0] + " and " + args[1]);
-//		}
-//		else if (args[3].equals("scorelog")) {
-//			Set<Integer> scoreSet = new HashSet<Integer>();
-//			String[] scores = args[7].split(",");
-//			for (String score : scores) {
-//				scoreSet.add(Integer.valueOf(score));
-//			}
-////			String[] logFiles = args[4].split(":");
-//			
-//			List<String> list = e.diagCluster(
-//					nodes,
-//					args[2],
-//					args[5], 
-//					args[6],
-//					scoreSet,
-//					args[9]
-//					);
-//			
-//			System.out.println("####### Begin ########");
-//			for (String n : list){
-//				System.out.println(n);
-//			}
-//			System.out.println("####### End ########");
-//			System.out.println(list.size() + " lines left.");
-//		}
+    	String[] nodes = args[8].split(",");
+    	extract e = new extract(args[0], args[1], args[2]);
+		if (args[3].equals("addtmp")) {
+			EsClient ec = new EsClient();
+			List<List<String>> snippet;
+			
+			try {
+				snippet = ec.getSnippet(nodes, args[2], args[5], args[6], "rws00fxw-cluster", "rws00fxw.us.oracle.com", 9300, args[9]);
+				List<String> content = snippet.get(0);
+				content = e.removeDupList(content);
+				Collections.sort(content);
+				System.out.println("Using " + content.size() + " lines of logs");
+				e.addTmp(content);
+				//return Snippet.scoreLog(judge.getScore(), snippet.get(1), logType, templatesFile, scoreSet);
+			} catch (UnknownHostException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			System.out.println("Adding templates completed, please check " + args[0] + " and " + args[1]);
+		}
+		else if (args[3].equals("scorelog")) {
+			Set<Integer> scoreSet = new HashSet<Integer>();
+			String[] scores = args[7].split(",");
+			for (String score : scores) {
+				scoreSet.add(Integer.valueOf(score));
+			}
+//			String[] logFiles = args[4].split(":");
+			
+			List<String> list = e.diagCluster(
+					nodes,
+					args[2],
+					args[5], 
+					args[6],
+					scoreSet,
+					args[9]
+					);
+			
+			System.out.println("####### Begin ########");
+			for (String n : list){
+				System.out.println(n);
+			}
+			System.out.println("####### End ########");
+			System.out.println(list.size() + " lines left.");
+		}
 		
 		
 		
