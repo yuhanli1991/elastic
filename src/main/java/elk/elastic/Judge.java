0aa3460baccebcfea0f35040c66e68358b9dce6c
yuhan.l.li <yuhan.l.li@Oracle.com>
2017-08-02 16:50:46 +0800

    Use iter to do getScore()

--- src/main/java/elk/elastic/Judge.java
+++ src/main/java/elk/elastic/Judge.java
@@ -24,17 +24,26 @@ public class Judge {
 		for (String line : list){
 //			if (line.length() > 1 && line.charAt(line.length() - 2) == '-' && line.charAt(line.length() - 3) == '-' && Character.isDigit(line.charAt(line.length() - 1))) {
 			if (Pattern.matches("^.*--[0-9]+$", line)) {
-				String[] sp = line.split("--");
-				String content = "";
-				if (sp.length > 2) {		//deal with 'abc-----------0'
-					for (int i = 0; i < sp.length - 2; i ++){
-						content += sp[i] + "--";
+//				String[] sp = line.split("--");
+//				String content = "";
+//				if (sp.length > 2) {		//deal with 'abc-----------0'
+//					for (int i = 0; i < sp.length - 2; i ++){
+//						content += sp[i] + "--";
+//					}
+//					content += sp[sp.length - 2];
+//				}
+//				else
+//					content = sp[0];
+//				score = Integer.valueOf(sp[sp.length - 1]);
+				int tail = -1;
+				for (int i = line.length() - 1; i >= 0; i --) {
+					if (!Character.isDigit(line.charAt(i))) {
+						tail = i - 1;
 					}
-					content += sp[sp.length - 2];
 				}
-				else
-					content = sp[0];
-				score = Integer.valueOf(sp[sp.length - 1]);
+				String content = line.substring(0, tail);
+				score = Integer.valueOf(line.substring(tail + 2, line.length()));
+				
 					//score = Integer.valueOf(Character.toString(line.charAt(line.length() - 1)));
 				try {
 					jo.put(content, score);
