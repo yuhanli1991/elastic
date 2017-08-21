d26e0ac6f8fade862f64c0780782540bc4abb9b3
Yuhan Li <liyuhanxmu@hotmail.com>
2017-08-21 15:37:35 +0800

    add buildMatrix debug

--- src/main/java/elk/elastic/EsClient.java
+++ src/main/java/elk/elastic/EsClient.java
@@ -168,7 +168,7 @@ public class EsClient {
 				.must(matchQuery("host", node[0]))
 //				.must(regexpQuery("path", ".*" + logType + ".*"));
 				.must(matchQuery("log_type", logType))
-				.must(matchQuery("path", "*" + path));
+				.must(matchQuery("path", "*log//." + path));
 		
 		try {
 			SearchResponse response = client.prepareSearch(index)
