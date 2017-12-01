ede54d9caf215c8aac1c6a0eba7381b695c4d52e
yuhan.li <liyuhanxmu@hotmail.com>
2017-12-01 21:11:52 +0800

    better replaceRawAsTmp

--- src/main/java/elk/elastic/EsClient.java
+++ src/main/java/elk/elastic/EsClient.java
@@ -201,7 +201,7 @@ public class EsClient {
 				.must(matchQuery("host", node[0]))
 //				.must(regexpQuery("path", ".*" + logType + ".*"));
 				.must(matchQuery("log_type", logType))
-				.must(matchQuery("path", path));
+				.must(regexpQuery("path", path));
 		
 		try {
 			SearchResponse response = client.prepareSearch(index)
--- src/main/java/elk/elastic/extract.java
+++ src/main/java/elk/elastic/extract.java
@@ -1117,7 +1117,8 @@ public class extract {
 		for (int i = 1; i <= 216; i ++) {
 			try {
 				System.out.println("===== " + Integer.toString(i) + " =====");
-				String path = this.fileNameMap.get(i - 1).split("\\.")[1] + "." + this.fileNameMap.get(i - 1).split("\\.")[2];
+				//String path = this.fileNameMap.get(i - 1).split("\\.")[1] + "." + this.fileNameMap.get(i - 1).split("\\.")[2];
+				String path = Integer.toString(i);
 //				if (path.equals("")) {
 //					throw new java.lang.RuntimeException("Path: " + Integer.toString(i) + " don't have related file name in ./fileName.txt");
 //				}
